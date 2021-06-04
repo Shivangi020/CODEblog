c8ef6c75ef96b8c6567ce4ba55c41c90cd9ab2df
Shivangi Singh <me.shivasingh.0987@gmail.com>
2021-06-04 16:59:14 +0530

    adding comment file

--- src/Components/comment/comment.js
+++ src/Components/comment/comment.js
@@ -1,9 +1,9 @@
-import React from 'react'
-import "./comment.css"
+import React from "react";
+import "./comment.css";
 import image from "../../images/image.jpg";
-import { FaAngleLeft} from 'react-icons/fa';
-import { FaAngleRight} from 'react-icons/fa';
-import { FaComment} from 'react-icons/fa';
+import { FaAngleLeft } from "react-icons/fa";
+import { FaAngleRight } from "react-icons/fa";
+import { FaComment } from "react-icons/fa";
 
 /**
 * @author
@@ -11,108 +11,94 @@ import { FaComment} from 'react-icons/fa';
 
 **/
 
-const Comment
- = (props) => {
-  return(
+const Comment = (props) => {
+  return (
     <div className="comment_header">
-     <nav className="nav_box nav_link">
-         <div className="nav1 nav_pad">
-         <a href="">
-             <span className="nav-label">
-                 <i className="icon1"></i>
-                 <span><FaAngleLeft/>Previous Article</span>
-             </span>
-             <span className="nav_img h4">
-                 <img style={{width:50,height:50}} src={image} className="img1"/>
-                 <span className="tittle">What Does It Take for a Fresher to Get an IT Job?</span>
-             </span>
-         </a>
-         </div>
+      <nav className="nav_box nav_link">
+        <div className="nav1 nav_pad">
+          <a href="">
+            <span className="nav-label">
+              <i className="icon1"></i>
+              <span>
+                <FaAngleLeft />
+                Previous Article
+              </span>
+            </span>
+            <span className="nav_img h4">
+              <img
+                style={{ width: 50, height: 50 }}
+                src={image}
+                className="img1"
+              />
+              <span className="tittle">
+                What Does It Take for a Fresher to Get an IT Job?
+              </span>
+            </span>
+          </a>
+        </div>
 
-          <div className="nav2 nav_pad">
-         <a href="">
-             <span className="nav-label">
-                 <i className="icon2"></i>
-                 <span>Next Article<FaAngleRight/></span>
-             </span>
-             <span className="nav_img h4">
-                 <img style={{width:50,height:50 }} src={image} className="img2" />
-                 <span className="tittle">What Does It Take for a Fresher to Get an IT Job?</span>
-             </span>
-         </a>
-         </div> 
-     </nav>
-     {/* nav bar ends here */}
-     
-     
-     
-     {/* Comment form */}
+        <div className="nav2 nav_pad">
+          <a href="">
+            <span className="nav-label">
+              <i className="icon2"></i>
+              <span>
+                Next Article
+                <FaAngleRight />
+              </span>
+            </span>
+            <span className="nav_img h4">
+              <img
+                style={{ width: 50, height: 50 }}
+                src={image}
+                className="img2"
+              />
+              <span className="tittle">
+                What Does It Take for a Fresher to Get an IT Job?
+              </span>
+            </span>
+          </a>
+        </div>
+      </nav>
+      {/* nav bar ends here */}
 
-     <aside className="comment_box">
-         <div className="comment_box_header clearfix">
-             <h4 className="reply">
-                 <i className="reply_icon"></i>
-                 Leave a reply
-             </h4>
-         </div>
-         <div className="comment-box-content">
-             <div id="comments" className="comment_area">
-              <div>
-                  <form className="comment-form">
-                      <p className="comment_notes">
-                          <span id="e-notes">Your email address will not be published.</span>
-                           <t />Required fields are mark
-                          <span className="required">*</span>
-                      </p>
-                      <p className="form-comment">
-                          <label for="comment"></label>
-                          <textarea placeholder="Leave Your Comment"
-                          required="required" className="user_comment">
+      {/* Comment form */}
 
-                          </textarea>
-                      </p>
-                    <div className="user_input">
-                      <p className="form-author">
-                          <label for="name"></label>
-                          <textarea placeholder="Name"
-                          required="required">
+      <aside className="comment_box">
+        <div className="comment_box_header clearfix">
+          <h4 className="reply">
+            <i className="reply_icon"></i>
+            Leave a reply
+          </h4>
+        </div>
 
-                          </textarea>
-                      </p>
-                     
-                <p className="form-email">
-                          <label for="email"></label>
-                          <textarea placeholder="Email"
-                          required="required" style={{marginRight:6}}>
-
-                          </textarea>
-                      </p>
-                      
-                    <p className="form-website">
-                          <label for="Website"></label>
-                          <textarea placeholder="Website"
-                          required="required" style={{marginRight:0}}>
-
-                          </textarea>
-                      </p></div>
-                      <p className="form-comment-cookies">
-                       <input type="checkbox" className="checkbox"/>
-                       <label for="comment-cookies">Save my name, email, and website in this browser for the next time I comment.</label>
-                     </p>
-                     <p className="submit">
-                       <input type="submit" className="submit_input" value="Post Comment"/>
-                       
-                     </p>
-                    
-                 </form>
-                 {/* commentform */}
-                 </div>   
-             </div>
-         </div>
-     </aside>
+        <div className="ContactForm">
+          <form>
+            <input type="text" required placeholder="Your Name"></input>
+            <input type="email" required placeholder="Your Email"></input>
+            <input type="number" required placeholder="Contact number"></input>
+            <textarea
+              required
+              placeholder="Type your Comment here*"
+              className="textareaContactDetails"
+              rows="4"
+            ></textarea>
+            <div class="form-comment-cookies">
+              <span>
+                <input type="checkbox" class="checkbox" />
+                <label for="comment-cookies">
+                  Save my name, email, and website in this browser for the next
+                  time I comment.
+                </label>
+              </span>
+            </div>
+            <button type="submit " className="btn-hover color-4">
+              Post Comment
+            </button>
+          </form>
+        </div>
+      </aside>
     </div>
-   )
-
- }
+  );
+};
 
 export default Comment;
